1f76593471466443defdd79ce0d9f3aa97a839e0
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2019-11-05 10:34:20 +0400

    Start using components

--- src/index.js
+++ src/index.js
@@ -3,21 +3,42 @@ import ReactDOM from "react-dom";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
-//создаём дерево элементов. Сложный элемент оборачиваем в ()
-const element = (
-  <div>
-    <h1>My Todo List</h1>
-    <input type="text" placeholder="search" />
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input type="text" placeholder="search" />;
+};
+
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Build Awesome App</li>
     </ul>
-  </div>
-);
+  );
+};
+
+//Собираем наше приложение из компонентов
+//JSX компоненты должны начинаться с Большой буквы, иначе они будут рассматриваться как обычный тег
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+//создаём дерево элементов. Сложный элемент оборачиваем в ()
+// const element = <App />;
 
 //Создание элемента без JSX. Именно в такой код переделывает jsx Babel
 // const element = React.createElement("h1", null, "Hello World");
 
-ReactDOM.render(element, document.getElementById("root"));
+//render на вход принимает именно компонент, а не имя элемента
+ReactDOM.render(<App />, document.getElementById("root"));
 
 console.log("Hello React");
