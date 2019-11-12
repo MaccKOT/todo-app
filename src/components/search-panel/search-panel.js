5108bd71ceb09ee906f195b5df968f09d1e4fde6
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2019-11-12 10:11:25 +0400

    Search filter function

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "./search-panel.css";
 
-const SearchPanel = () => {
-  const searchText = "Type here to search";
-  //передаём атрибут (свойство, properties) в реакт-элемент
-  //атрибуты html-элемента пишутся особым образом в camel-case. Например className для задания своего класса или htmlFor для for
-  const searchStyle = {
-    fontSize: "20px"
-  }; //передаём объект в качестве описания стилей
-  //если не передать значение атрибуту, то по умолчанию оно становится {true}
-  return (
-    <input
-      type="text"
-      className="form-control search-input"
-      placeholder={searchText}
-      style={searchStyle}
-      autoComplete=""
-      tabIndex={"1"}
-      disabled={false}
-    />
-  );
-};
+export default class SearchPanel extends Component {
+  state = {
+    term: ""
+  };
 
-export default SearchPanel;
+  onSearchChange = event => {
+    const term = event.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  };
+
+  render() {
+    const searchStyle = {
+      fontSize: "20px"
+    }; //передаём объект в качестве описания стилей
+
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="Type here to search"
+        style={searchStyle}
+        autoComplete=""
+        value={this.state.term}
+        onChange={this.onSearchChange}
+      />
+    );
+  }
+}
